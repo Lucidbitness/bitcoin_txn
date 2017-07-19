27eb8984c9d94bcef832c0842ccf5be917044473
Chris Barber <christopher.l.barber@gmail.com>
2017-07-18 18:10:04 -0600

    add block range sort to address pull

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
+global.fetch = require('node-fetch');
+const cc = require('cryptocompare');
+
 var bodyParser     = require("body-parser"),
 	request        = require('request'),
 	express        = require("express"),
 	app            = express();
+
+
 
 var bitcoin_address = "1DEP8i3QJCsomS4BSMY2RpU1upv62aGvhD";
 var txn_hash = "b357ef869a27affd4442e57367396dc404b5757da117d8903ef196fd021b57bc"; 
@@ -14,25 +19,32 @@ var txn_hash = "b357ef869a27affd4442e57367396dc404b5757da117d8903ef196fd021b57bc
 	});
 
 	app.get("/results", function(req, res){
-		request('https://api.blockcypher.com/v1/btc/main/addrs/'+bitcoin_address+'/full', function (error, response, body) {
+		var query = req.query.address;
+		var after = req.query.blockStart;
+		var before = req.query.blockEnd;
+		
+		request('https://api.blockcypher.com/v1/btc/main/addrs/'+query+'/full?before='+before+'&after='+after, function (error, response, body) {
   			console.log('error:', error); // Print the error if one occurred 
   			console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
   			//var result = JSON.parse(body);
-			res.send(body); // txx 0 hash "136c395e49c64eebe07d4fd540bc31a331c0a608a3afa9d35702ee8d55afe920"
+			res.send(body); // tx 0 hash "136c395e49c64eebe07d4fd540bc31a331c0a608a3afa9d35702ee8d55afe920"
 		});
 
-		// request('https://blockchain.info/q/txfee' + txn_hash, function (error, response, body) {
-  // 			console.log('error:', error); // Print the error if one occurred 
-  // 			console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
-		// 	res.send(body); // Print the HTML for the Google homepage.
-		// });
+		//request('https://min-api.cryptocompare.com/data/pricehistorical?fsym=BTC&tsyms=USD,EUR&ts=1452680400&extraParams=your_app_name') price chart
 
+		// Basic Usage: 
+		cc.priceHistorical('BTC', ['USD', 'EUR'], new Date('2017-07-01')) //run in foreach in middleware
+			.then(prices => {
+  		console.log(prices)
+  		// -> { BTC: { USD: 997, EUR: 948.17 } } 
+		})
+		.catch(console.error)
 	});
 
 var port = process.env.PORT || 4000;
 
 app.listen(port, function(){
-	console.log("BCard Server Open on 4000");
+	console.log("Bitcoin txn getter Open on 4000");
 });
 
 // <input type="file" accept="image/*;capture=camera">
